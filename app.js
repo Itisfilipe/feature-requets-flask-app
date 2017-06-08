a46648e14a7245f73a1d086053506cdd96493b37
Filipe T. Amaral <filipetamaral@gmail.com>
2017-06-08 00:35:45 -0300

    json-server replaced with flask server, bower replaced with npm

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
  */
 function modalControl() {
   // Events to control modal transitions
-  // Client modal
+  // New Client modal
   $('#newClientModal').on('show.bs.modal', function(e) {
     $('#newFeatureModal').modal('toggle');
   });
   $('#newClientModal').on('hidden.bs.modal', function(e) {
     $('#newFeatureModal').modal('toggle');
   });
-  // Product Area Modal
+  // New Product Area Modal
   $('#newProductAreaModal').on('show.bs.modal', function(e) {
     $('#newFeatureModal').modal('toggle');
   });
@@ -31,7 +31,6 @@ function Feature(data) {
   this.date = ko.observable(data.date);
   this.area = ko.observable(new ProductArea(data.productArea));
 }
-
 /**
  * Product Area model
  * @param data - Product Area document provided by the API
@@ -48,13 +47,14 @@ function Client(data) {
   this.id = data.id;
   this.name = data.name;
   this.priorities = [];
-  for(var i = 1; i <= +data.maxPriorities; i++){
+  for (var i = 1; i <= +data.maxPriorities; i++) {
     this.priorities.push(i)
   }
 }
 
 function FeaturesViewModel() {
   var self = this;
+  var URL = 'http://127.0.0.1:5000';
   // loaded data from server
   self.productAreas = ko.observableArray([]);
   self.clients = ko.observableArray([]);
@@ -66,49 +66,104 @@ function FeaturesViewModel() {
   self.newTitle = ko.observable();
   self.newDescription = ko.observable();
   self.newDate = ko.observable();
-  this.selectedClient = ko.observable();
-  this.selectedProductArea = ko.observable();
-  this.selectedPriority = ko.observable();
-
+  self.selectedClient = ko.observable();
+  self.selectedProductArea = ko.observable();
+  self.selectedPriority = ko.observable();
   // Behaviours
   self.goToFeature = function(feature) { location.hash = feature.id() };
   self.goHome = function() { location.hash = '#/' };
   // Operations
   self.addClient = function(client) {
-    var data = {name: self.newClientName()}
-    $.post("http://localhost:1234/clients", data).done(function(s) {
-      self.getClients();
-      self.newClientName("")
-    }).always(function(){ $('#newClientModal').modal('hide'); });;
+    var data = { name: self.newClientName() }
+    $.ajax({
+      type: "POST",
+      url: URL + "/clients",
+      datatype: 'json',
+      contentType: "application/json",
+      data: JSON.stringify(data),
+      success: function() {
+        self.getClients();
+        self.newClientName("");
+      }
+    }).always(function() { $('#newClientModal').modal('hide'); });;
   };
   self.addProductArea = function() {
-    var data = {name: self.newAreaName()}
-    $.post("http://localhost:1234/productAreas", data).done(function(s) {
-      self.getProductAreas();
-      self.newAreaName("");
-    }).always(function(){ $('#newProductAreaModal').modal('hide'); });
+    var data = { name: self.newAreaName() }
+    $.ajax({
+      type: "POST",
+      url: URL + "/product-areas",
+      datatype: 'json',
+      contentType: "application/json",
+      data: JSON.stringify(data),
+      success: function() {
+        self.getProductAreas();
+        self.newAreaName("");
+      }
+    }).always(function() { $('#newProductAreaModal').modal('hide'); });
   };
   self.addFeature = function() {
     var data = {
-      title: self.newTitle,
-      description: self.newDescription,
-      date: self.newDate,
+      title: self.newTitle(),
+      description: self.newDescription(),
+      date: self.newDate(),
       productAreaId: self.selectedProductArea().id,
       clientId: self.selectedClient().id,
       priority: self.selectedPriority()
     };
-    $.post("http://localhost:1234/features", data).done(function(s) {
-      self.newTitle("");
-      self.newDescription("");
-      self.newDate("");
-      self.selectedProductArea("");
-      self.selectedClient("");
-      self.selectedPriority("");
-      self.getFeatures();
-    }).always(function(){ $('#newFeatureModal').modal('hide'); });
+    var method = "POST"
+    URL_suffix = ""
+    if (self.featureDetails() !== null) {
+      URL_suffix = '/' + self.featureDetails().id;
+      method = "PATCH"
+    }
+    $.ajax({
+      type: method,
+      url: URL + "/features" + URL_suffix,
+      datatype: 'json',
+      contentType: "application/json",
+      data: JSON.stringify(data),
+      success: function(result) {
+        self.newTitle("");
+        self.newDescription("");
+        self.newDate("");
+        self.selectedProductArea("");
+        self.selectedClient("");
+        self.selectedPriority("");
+        self.getFeatures();
+      }
+    }).always(function() { $('#newFeatureModal').modal('hide'); });
   };
-  self.getPriorities = function(){
-    $.getJSON("http://localhost:1234/clients", function(data) {
+  self.deleteFeature = function(data) {
+    if (!confirm("Are you sure?")) return;
+    $.ajax({
+      type: "DELETE",
+      url: URL + "/features/" + data.id,
+      success: function(result) {
+        self.featureDetails(null);
+        self.getFeatures();
+        self.getProductAreas();
+        self.getClients();
+        self.goHome();
+      }
+    });
+  }
+  self.editFeature = function(data) {
+    self.newTitle(data.title);
+    self.newDescription(data.description);
+    self.newDate(data.date);
+    var client = self.clients().filter(function(client) {
+      return client.id === +data.client.id
+    })[0];
+    self.selectedClient(client);
+    var area = self.productAreas().filter(function(area) {
+      return area.id === +data.productArea.id
+    })[0];
+    self.selectedProductArea(area);
+    self.selectedPriority(data.priority);
+    $('#newFeatureModal').modal('show');
+  }
+  self.getPriorities = function() {
+    $.getJSON(URL + "/clients", function(data) {
       var mappedClients = $.map(data, function(clientData) {
         return new Client(clientData)
       });
@@ -116,7 +171,7 @@ function FeaturesViewModel() {
     });
   }
   self.getClients = function() {
-    $.getJSON("http://localhost:1234/clients", function(data) {
+    $.getJSON(URL + "/clients", function(data) {
       var mappedClients = $.map(data, function(clientData) {
         return new Client(clientData)
       });
@@ -124,7 +179,7 @@ function FeaturesViewModel() {
     });
   }
   self.getProductAreas = function() {
-    $.getJSON("http://localhost:1234/productAreas", function(data) {
+    $.getJSON(URL + "/product-areas", function(data) {
       var mappedAreas = $.map(data, function(areaData) {
         return new ProductArea(areaData);
       });
@@ -132,7 +187,7 @@ function FeaturesViewModel() {
     });
   }
   self.getFeatures = function() {
-    $.getJSON("http://localhost:1234/features?_expand=client&_expand=productArea", function(data) {
+    $.getJSON(URL + "/features", function(data) {
       var mappedFeatures = $.map(data, function(featureData) {
         return new Feature(featureData)
       });
@@ -140,21 +195,23 @@ function FeaturesViewModel() {
     });
   }
   self.getFeature = function(featureId) {
-    $.getJSON("http://localhost:1234/features/" + featureId, self.featureDetails);
-  }
-  // Routes
+      $.getJSON(URL + "/features/" + featureId, self.featureDetails);
+    }
+    // Routes
   Sammy(function() {
     // feature details
     this.get('#:featureId', function() {
       self.features(null);
       self.getFeature(this.params.featureId);
+      self.getProductAreas();
+      self.getClients();
     });
     // index/features list
     this.get('', function() {
       self.featureDetails(null);
       self.getFeatures();
-      self.getProductAreas()
-      self.getClients()
+      self.getProductAreas();
+      self.getClients();
     });
   }).run();
 };
